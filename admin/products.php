d477b79964f40e280ba80f5381fcefeecd65d9dc
LHoRrrr <lihorcuteboy007@gmail.com>
2025-03-04 21:34:18 +0700

    create add product form in product.php

--- admin/products.php
+++ admin/products.php
@@ -1,69 +1,78 @@
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+<div class="form-container-add-product">
+    <h1 class="">ADD PRODUCT FORM</h1>
+    <div class="form-body-add-product">
+        <form action="products.php">
+            <div class="product-general-information-container">
+                <h4>General Information</h4>
 
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Charts</h1>
-                    <p class="mb-4">Chart.js is a third party plugin that is used to generate the charts in this theme.
-                        The charts below have been customized - for further customization options, please visit the <a
-                            target="_blank" href="https://www.chartjs.org/docs/latest/">official Chart.js
-                            documentation</a>.</p>
+                <div class="product-name-container">
+                    <label for="">Product ID</label> </br>
+                    <input type="text" placeholder="Enter product ID" name="product_id"> </br>
+                    <label for="">Prodcut Name</label> </br>
+                    <input type="text" placeholder="Enter product name" name="product_name"> </br>
+                </div>
 
-                    <!-- Content Row -->
-                    <div class="row">
+                <div class="product-specs-container">
+                    <div class="product-specs">
+                        <label for="">Specs</label> </br>
+                        <input type="text" name="product_specs" placeholder="Product specs">
+                    </div>
 
-                        <div class="col-xl-8 col-lg-7">
+                    <div class="product-description">
+                        <label for="">Short description</label> </br>
+                        <textarea type="text" name = "product_description" cols= "50" rows="10" > </textarea>
+                    </div>
+                </div>
 
-                            <!-- Area Chart -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
-                                    </div>
-                                    <hr>
-                                    Styling for the area chart can be found in the
-                                    <code>/js/demo/chart-area-demo.js</code> file.
-                                </div>
-                            </div>
+                <div class="product-brand-container">
+                    <div class="product-brAand">
+                        <label for="">Product brand</label> </br>
+                        <input type="text" name="product_brand" placeholder="Example: Apple...">
+                    </div>
+                    <div class="prodcut-mark">
+                        <label for="">Product Mark</label> </br>
+                        <select name="product_mark" id="">
+                            <option value=""></option>
+                            <option value="New release">New Releasing</option>
+                            <option value="New">New</option>
+                            <option value="Second hand">Second Hand</option>
+                        </select>
+                    </div>
 
-                            <!-- Bar Chart -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-bar">
-                                        <canvas id="myBarChart"></canvas>
-                                    </div>
-                                    <hr>
-                                    Styling for the bar chart can be found in the
-                                    <code>/js/demo/chart-bar-demo.js</code> file.
-                                </div>
-                            </div>
-
+                    <div class="product-price-container">
+                        <div class="product-price">
+                            <label for="">Price</label> </br>
+                            <input type="text" name="product_price" placeholder="Product price">
                         </div>
-
-                        <!-- Donut Chart -->
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="chart-pie pt-4">
-                                        <canvas id="myPieChart"></canvas>
-                                    </div>
-                                    <hr>
-                                    Styling for the donut chart can be found in the
-                                    <code>/js/demo/chart-pie-demo.js</code> file.
-                                </div>
-                            </div>
+                        <div class="prodcut-quantity">
+                            <label for="">Quantity</label></br>
+                            <input type="text" name="prodcut_quantity" placeholder="Product quantity">
+                        </div>
+                        <div class="product-status">
+                            <label for="">Status</label> </br>
+                            <select name="product_status" id="">
+                                <option value="">Available</option>
+                                <option value="">Out of Stock</option>
+                            </select>
+                        </div>
+                        <div class="product-order">
+                            <label for="">Order</label></br>
+                            <input type="number" name="product_order" placeholder="Product order number">
                         </div>
                     </div>
 
                 </div>
-                <!-- /.container-fluid -->
+
+
+            </div>
+            <div class="add-image-product-container">
+                <h4>Product Image</h4>
+                <div class="prodcut-image-container">
+                    
+                </div>
+                <input type="file" name="product_image">
+            </div>
+
+        </form>
+    </div>
+</div>
